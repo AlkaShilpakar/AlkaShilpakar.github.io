4e84279865d32c051007ceb8568df509ff953950
Alka Shilpakar <alshu.shilp@gmail.com>
2019-02-12 14:08:50 +0545

    Art Gallery

--- adminpanel/includes/header.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title></title>
-        <link rel="stylesheet" type="text/css" href="../asset/css/bootstrap.css"> 
-        <link rel="stylesheet" type="text/css" href="../asset/css/style.css">
-        <link rel="stylesheet" type="text/css" href="../asset/css/all.css">
-    </head>
-    <body>
-
